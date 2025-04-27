0c4dc78a399874fdfe50034db4a381bad1ff9a40
jai <jaiakash2907@gmail.com>
2025-04-27 10:03:49 +0530

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 
 let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }]
