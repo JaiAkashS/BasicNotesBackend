6895caf34a2fc7ad01ccc761cc0c58f6f6deced2
jai <jaiakash2907@gmail.com>
2025-04-27 21:37:47 +0530

    added mongo atlas

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
+require('dotenv').config()
+const Note = require('./models/note')
 const express = require('express')
-const cors = require('cors')
 const app = express()
+
 
 
 app.use(express.static('dist'))
 app.use(express.json())
 
-let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }]
-
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello World!</h1>')
-})
-
-
 app.get('/api/notes',(req,res)=>{
-    res.json(notes)
+    Note.find({}).then( notes =>{
+      res.json(notes)
+    }
+    )
 })
 
 
@@ -36,14 +34,8 @@ app.delete('/api/notes/:id',(req,res)=>{
     res.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
   
-  app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
     const body = request.body
   
     if (!body.content) {
@@ -52,17 +44,15 @@ const generateId = () => {
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+    })
+    note.save().then(saved=>{
+      console.log(saved,'has been saved!')
+    })
 })
 
-const PORT = process.env.PORT || 6969
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
