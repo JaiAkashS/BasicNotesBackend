4eb088e69b4845857c2ae5b504a6cd133ace7965
jai <jaiakash2907@gmail.com>
2025-04-28 20:40:03 +0530

    uibuild

--- index.js
+++ index.js
@@ -43,22 +43,18 @@ app.delete('/api/notes/:id',(req,res,next)=>{
 })
 
   
-app.post('/api/notes', (request, response) => {
-    const body = request.body
+app.post('/api/notes', (req,res,next) => {
+    const body = req.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
   
     const note = new Note({
       content: body.content,
       important: body.important || false,
     })
     note.save().then(saved=>{
-      response.json(saved)
+      res.json(saved)
     })
+    .catch(error=>next(error))
 })
 
 
@@ -86,6 +82,9 @@ const errorHandler = (error,request,response,next) =>{
   console.log(error)
   if(error.name === 'CastError'){
     return response.status(404).send({error:'malformed Id'})
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).send({error:error.message})
   }
   next(error)
 }
