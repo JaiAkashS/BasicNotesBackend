b46b5ee32928f029797dacfb8a91b6bca3dee355
jai <jaiakash2907@gmail.com>
2025-04-28 21:27:36 +0530

    Added Eslint and its configs

--- index.js
+++ index.js
@@ -8,83 +8,83 @@ const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 
-app.get('/api/notes',(req,res)=>{
-    Note.find({}).then( notes =>{
-      res.json(notes)
-    }
-    )
+app.get('/api/notes',(req,res) => {
+  Note.find({}).then( notes => {
+    res.json(notes)
+  }
+  )
 })
 
 
 
-app.get('/api/notes/:id',(req,res,next)=>{
+app.get('/api/notes/:id',(req,res,next) => {
   Note.findById(req.params.id)
-  .then(note => {
-    if (note) {        
-      res.json(note)      
-    } else {        
-      res.status(404).end()      
-  }})
-  .catch(error => { next(error)
-})
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }})
+    .catch(error => { next(error)
+    })
 
 })
 
-app.delete('/api/notes/:id',(req,res,next)=>{
+app.delete('/api/notes/:id',(req,res,next) => {
   Note.findByIdAndDelete(req.params.id)
-  .then(result=>{
-    if(result){console.log("Note has been deleted result:",result)
-    res.status(204).end()}
-    else{
-      res.status(404).send({error:"Note is not found"})
-    }
-  })
-  .catch(error=>next(error))
+    .then(result => {
+      if(result){console.log('Note has been deleted result:',result)
+        res.status(204).end()}
+      else{
+        res.status(404).send({ error:'Note is not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
-  
+
 app.post('/api/notes', (req,res,next) => {
-    const body = req.body
-  
-  
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-    })
-    note.save().then(saved=>{
-      res.json(saved)
-    })
-    .catch(error=>next(error))
+  const body = req.body
+
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(saved => {
+    res.json(saved)
+  })
+    .catch(error => next(error))
 })
 
 
-app.put('/api/notes/:id',(req,res,next)=>{
-  const {content , important} = req.body
+app.put('/api/notes/:id',(req,res,next) => {
+  const { content , important } = req.body
 
   Note.findById(req.params.id)
-  .then(note =>{
-    if(!note){
-      return res.status(404).send({error:'Note not found'})
-    }
-    note.content = content
-    note.important = important
-    
-    return note.save().then((newNote)=>{
-      res.json(newNote)
-    })
-  }).catch(error => next(error))
+    .then(note => {
+      if(!note){
+        return res.status(404).send({ error:'Note not found' })
+      }
+      note.content = content
+      note.important = important
+
+      return note.save().then((newNote) => {
+        res.json(newNote)
+      })
+    }).catch(error => next(error))
 
 })
 
 
 
-const errorHandler = (error,request,response,next) =>{
+const errorHandler = (error,request,response,next) => {
   console.log(error)
   if(error.name === 'CastError'){
-    return response.status(404).send({error:'malformed Id'})
+    return response.status(404).send({ error:'malformed Id' })
   }
   else if(error.name === 'ValidationError'){
-    return response.status(400).send({error:error.message})
+    return response.status(400).send({ error:error.message })
   }
   next(error)
 }
