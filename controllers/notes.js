ee01705f355f44624359edf0e3b76f897df7d312
jai <jaiakash2907@gmail.com>
2025-05-05 21:15:58 +0530

    Added Users model and some routes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 
 notesRouter.get('/',async (req,res) => {
@@ -34,12 +35,18 @@ notesRouter.delete('/:id', async (req,res) => {
 notesRouter.post('/', async (req,res) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user:user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   res.status(201).json(savedNote)
 
 })
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,7 +5,8 @@ const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_help')
 const Note = require('../models/note')
-
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
 const api = supertest(app)
 
 describe('when there is initially some notes saved', () => {
@@ -107,6 +108,43 @@ describe('when there is initially some notes saved', () => {
     })
   })
 })
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('password', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+})
+
+
+
+
 
 after(async () => {
   await mongoose.connection.close()
